7e8e30736fe0b3ad0c0123dae6f7853713eee5f6
CodeWithAbdur <106903848+codewithabdur@users.noreply.github.com>
2023-11-29 11:27:55 +0530

    Add files via upload

---
+++ iujgf.py
@@ -0,0 +1,17 @@
+# pip install pyperclip
+# pip install pyautogui
+# pip install keyboard
+
+import pyperclip
+import pyautogui
+import webbrowser
+import keyboard
+from time import sleep
+
+sleep(20)
+print(pyautogui.position())
+
+
+# x=1730, y=64
+# x=1515, y=246
+# x=1449, y=98
